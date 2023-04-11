ebd6e71eee0890c69637b7c8efd7f586e01e65a1
shubhamechikitsalaya <shubham.agarwal@echikitsalaya.com>
2023-04-11 20:42:57 +0530

    typing

--- src/chatWindow.js
+++ src/chatWindow.js
@@ -1,16 +1,17 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useState, useEffect, useRef } from "react";
-const Header = ({ name, profile_pic }) => (
+const Header = ({ name, profile_pic, typing }) => (
   <div className="Header">
     <img className="Header-img" src={profile_pic}></img>
     <h3 className="Header-details">{name}</h3>
+    <div className={typing && "typing"}></div>
   </div>
 );
 
 const Messages = ({ messages }) => {
   const myref = useRef(null);
   const data = Object.values(messages);
-
+  console.log(myref);
   useEffect(() => {
     myref.current.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
@@ -18,10 +19,11 @@ const Messages = ({ messages }) => {
   return (
     <div className="Messages">
       {data.map((e) => (
-        <div ref={myref} className={e.is_user_msg ? "user-msg" : "non-user-msg"} key={e.number}>
+        <div className={e.is_user_msg ? "user-msg" : "non-user-msg"} key={e.number}>
           {e.text}
         </div>
       ))}
+      <div ref={myref} />
     </div>
   );
 };
@@ -35,22 +37,31 @@ const InputBox = ({ activeUserId }) => {
       onSubmit={(e) => {
         e.preventDefault();
         dispatch({ type: "SET_MESSAGE", payload: { activeUserId, text: input } });
+        dispatch({ type: "STOP_TYPING" });
         setInput("");
       }}
     >
-      <input className="InputBox-element" value={input} onChange={(e) => setInput(e.target.value)} />
+      <input
+        className="InputBox-element"
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+          dispatch({ type: "SET_TYPING" });
+        }}
+      />
     </form>
   );
 };
 
 const ChatWindow = () => {
+  const typing = useSelector((s) => s.typing);
   const activeId = useSelector((s) => s.activeUserId);
   const { name, profile_pic } = useSelector((s) => s.contacts[activeId]);
   const messages = useSelector((s) => s.messages[activeId]);
 
   return (
     <div className="ChatWindow">
-      <Header name={name} profile_pic={profile_pic} />
+      <Header name={name} profile_pic={profile_pic} typing={typing} />
       <Messages messages={messages} />
       <InputBox activeUserId={activeId} />
     </div>
--- src/sidebar.js
+++ src/sidebar.js
@@ -2,16 +2,11 @@ import { useSelector, useDispatch } from "react-redux";
 const Sidebar = () => {
   const data = Object.values(useSelector((s) => s.contacts));
   const dispatch = useDispatch();
-  console.log(useSelector((s) => s));
   return (
     <div className="Sidebar">
       {data.map((e) => {
         return (
-          <div
-            className="User"
-            key={e.name}
-            onClick={() => dispatch({ type: "SET_ACTIVE_USER", payload: e.user_id })}
-          >
+          <div className="User" key={e.name} onClick={() => dispatch({ type: "SET_ACTIVE_USER", payload: e.user_id })}>
             <img className="User-img" src={e.profile_pic}></img>
             <div className="User-details">
               <h3>{e.name}</h3>
